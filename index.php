eca3144b20cbd387a88ccd17dfd6add1046f18f2
ESTON-kiam <engestonbrandonkiama@gmail.com>
2024-11-13 15:52:05 +0300

    update

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Make Payment - Nitumie Bob</title>
+    <link href="images/1200px-M-PESA_LOGO-01.svg.png" rel="icon">
+    <link href="images/1200px-M-PESA_LOGO-01.svg.png" rel="apple-touch-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
